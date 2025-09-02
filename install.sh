b93ac470329acbf057f8cd1457a5dae7acf5b4d8
Hamed Kouhfallah <54766086+voipiran@users.noreply.github.com>
2025-09-02 14:08:00 +0330

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+clear
+# Colorize output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+MAGENTA='\033[1;35m'
+BOLD='\033[1m'
+NC='\033[0m' # No color
+echo -e "${MAGENTA}###############################################################${NC}"
+echo -e "${CYAN}██╗   ██╗ ██████╗ ██╗██████╗ ██╗██████╗  █████╗ ███╗   ██╗${NC}"
+echo -e "${CYAN}██║   ██║██╔═══██╗██║██╔══██╗██║██╔══██╗██╔══██╗████╗  ██║${NC}"
+echo -e "${CYAN}██║   ██║██║   ██║██║██████╔╝██║██████╔╝███████║██╔██╗ ██║${NC}"
+echo -e "${CYAN}╚██╗ ██╔╝██║   ██║██║██╔═══╝ ██║██╔══██╗██╔══██║██║╚██╗██║${NC}"
+echo -e "${CYAN} ╚████╔╝ ╚██████╔╝██║██║     ██║██║  ██║██║  ██║██║ ╚████║${NC}"
+echo -e "${CYAN}  ╚═══╝   ╚═════╝ ╚═╝╚═╝     ╚═╝╚══╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝${NC}"
+echo -e "${MAGENTA}###############################################################${NC}"
+echo -e "${MAGENTA}                    https://voipiran.io                    ${NC}"
+echo -e "${MAGENTA}###############################################################${NC}"
+
+echo "Install VOIPIRAN WebPhone"
+echo "VOIPIRAN.io"
+echo "VOIPIRAN WebPhone 1.0"
+sleep 1
+
 issabel-menumerge /tmp/VOIZ-WebPhone/phone.xml
 
 # اضافه کردن تنظیمات به انتهای فایل sip_custom.conf
